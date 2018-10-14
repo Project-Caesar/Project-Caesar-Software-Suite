ce9d6b7b5e717f01e424e5342ca373660d5324f4
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-10-14 17:24:44 -0400

    Added comments, checked if an image was selected, and set image base size

--- src/main/kotlin/games/TrainingWheelsMenu.kt
+++ src/main/kotlin/games/TrainingWheelsMenu.kt
@@ -7,8 +7,9 @@ import javafx.scene.layout.HBox
 import javafx.stage.FileChooser
 import javafx.stage.Stage
 import tornadofx.*
-import java.io.File
 
+// main and app used to test the menu
+// will be removed when menu is complete
 fun main(args: Array<String>) {
     launch<TrainingWheelsApp>(args)
 }
@@ -26,6 +27,9 @@ class TrainingWheelsApp : App(TrainingWheelsMenu::class) {
 }
 class TrainingWheelsMenu : View() {
 
+    // used to select an icon file from a directory
+    private val fileChooser = FileChooser()
+    // used to store selected image
     private val selectedIconPreview = SimpleObjectProperty<Image>()
 
     override val root = HBox()
@@ -35,7 +39,13 @@ class TrainingWheelsMenu : View() {
             vbox {
                 button("Select Icon") {
                     setOnAction {
-                        val fileChooser = FileChooser()
+                        /**
+                         * when clicked/touched, add an image file extension filter to the
+                         * file chooser and use it to select a valid file to be used as the icon
+                         *
+                         * the file is then converted into an image, and that image is stored in the
+                         * simple image property
+                         */
 
                         val imageFilter = FileChooser.ExtensionFilter(
                                 "Select Icon",
@@ -44,13 +54,27 @@ class TrainingWheelsMenu : View() {
 
                         fileChooser.extensionFilters += imageFilter
                         val file = fileChooser.showOpenDialog(null)
-                        println(file)
-                        selectedIconPreview.value = Image(file.toURI().toString())
+                        if (file != null) {
+                            println(file)
+                            selectedIconPreview.value = Image(file.toURI().toString())
+                        }
                     }
                 }
 
                 imageview {
+                    // the imageview image will always be what is stored in the simple image property
                     imageProperty().bind(selectedIconPreview)
+
+                    // set image dimensions when one is selected
+                    imageProperty().onChange {
+                        if (image.width >= image.height) {
+                            fitWidth = 100.0
+                            fitHeight = (fitWidth / image.width) * image.height
+                        } else {
+                            fitHeight = 100.0
+                            fitWidth = (fitHeight / image.height) * image.width
+                        }
+                    }
                 }
             }
         }
