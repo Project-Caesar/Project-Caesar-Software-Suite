e5b487f050def4be529ceeca807e6b35788481be
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-10-21 14:56:21 -0400

    Added PercentInputField widget to accepts a percentage value between a max and min. Set up values for number of clicks to shrink, the size at which the image starts moving, and the starting size of the image. Also added a button which will eventually start the trianingwheels if the values entered are acceptable.

--- src/main/kotlin/games/TrainingWheelsMenu.kt
+++ src/main/kotlin/games/TrainingWheelsMenu.kt
@@ -1,17 +1,17 @@
 package games
 
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleDoubleProperty
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleObjectProperty
 import javafx.geometry.Pos
 import javafx.scene.image.Image
 import javafx.scene.input.KeyCombination
-import javafx.scene.layout.HBox
-import javafx.scene.text.Font
-import javafx.scene.text.FontWeight
+import javafx.scene.layout.VBox
 import javafx.stage.FileChooser
 import javafx.stage.Stage
 import tornadofx.*
+import widgets.PercentInputField
 
 // main and app used to test the menu
 // will be removed when menu is complete
@@ -38,119 +38,132 @@ class TrainingWheelsMenu : View() {
     // used to store selected image
     private val selectedIconPreview = SimpleObjectProperty<Image>()
 
-    private val iconShrinkRate = SimpleIntegerProperty(1)
-    private val iconShrinkLimit = SimpleDoubleProperty(.10)
+    private val iconClicksToShrink = SimpleIntegerProperty(1)
+    private val iconShrinkLimit = SimpleDoubleProperty(.1)
+    private val iconStartSize = SimpleDoubleProperty(.9)
 
-    override val root = HBox()
+
+    //private val iconShrinkRatio = SimpleDoubleProperty(.9)
+
+    private val readyToStart = SimpleBooleanProperty(true)
+
+    override val root = VBox()
 
     init {
+
+        iconShrinkLimit.onChange {
+            if (it > iconStartSize.value) {
+                readyToStart.set(false)
+            } else {
+                readyToStart.set(true)
+            }
+        }
+
+        iconStartSize.onChange {
+            if (it < iconShrinkLimit.value) {
+                readyToStart.set(false)
+            } else {
+                readyToStart.set(true)
+            }
+        }
+
         with(root) {
             alignment = Pos.CENTER
 
-            vbox {
-                button("Select Icon") {
-                    setOnAction {
-                        /**
-                         * when clicked/touched, add an image file extension filter to the
-                         * file chooser and use it to select a valid file to be used as the icon
-                         *
-                         * the file is then converted into an image, and that image is stored in the
-                         * simple image property
-                         */
+            hbox {
 
-                        val imageFilter = FileChooser.ExtensionFilter(
-                                "Select Icon",
-                                listOf("*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif")
-                        )
+                vbox {
+                    button("Select Icon") {
+                        setOnAction {
+                            /**
+                             * when clicked/touched, add an image file extension filter to the
+                             * file chooser and use it to select a valid file to be used as the icon
+                             *
+                             * the file is then converted into an image, and that image is stored in the
+                             * simple image property
+                             */
 
-                        fileChooser.extensionFilters += imageFilter
-                        val file = fileChooser.showOpenDialog(null)
-                        if (file != null) {
-                            println(file)
-                            selectedIconPreview.value = Image(file.toURI().toString())
-                        }
-                    }
-                }
+                            val imageFilter = FileChooser.ExtensionFilter(
+                                    "Select Icon",
+                                    listOf("*.png", "*.jpg", "*.jpeg", "*.bmp", "*.gif")
+                            )
 
-                imageview {
-                    // the imageview image will always be what is stored in the simple image property
-                    imageProperty().bind(selectedIconPreview)
-
-                    // set image dimensions when one is selected
-                    imageProperty().onChange {
-                        if (image.width >= image.height) {
-                            fitWidth = 100.0
-                            fitHeight = (fitWidth / image.width) * image.height
-                        } else {
-                            fitHeight = 100.0
-                            fitWidth = (fitHeight / image.height) * image.width
-                        }
-                    }
-                }
-            }
-
-            vbox {
-                /**
-                 * Need to be able to set number of successes until shrink, and at which size it starts moving
-                 */
-
-                textfield {
-                    alignment = Pos.CENTER_RIGHT
-                    text = "1"
-
-                    textProperty().onChange {
-                        if (it == null) {
-                            iconShrinkRate.set(1)
-                        } else {
-                            it.trim()
-                            if (it.isInt() && it.toInt() >= 1) {
-                                iconShrinkRate.set(it.toInt())
-                            } else {
-                                iconShrinkRate.set(1)
+                            fileChooser.extensionFilters += imageFilter
+                            val file = fileChooser.showOpenDialog(null)
+                            if (file != null) {
+                                println(file)
+                                selectedIconPreview.value = Image(file.toURI().toString())
                             }
                         }
                     }
 
-                    focusedProperty().onChange {
-                        if (!it) {
-                            text = iconShrinkRate.value.toString()
+                    imageview {
+                        // the imageview image will always be what is stored in the simple image property
+                        imageProperty().bind(selectedIconPreview)
+
+                        // basic image dimensions when one is selected
+                        imageProperty().onChange {
+                            if (image.width >= image.height) {
+                                fitWidth = 100.0
+                                fitHeight = (fitWidth / image.width) * image.height
+                            } else {
+                                fitHeight = 100.0
+                                fitWidth = (fitHeight / image.height) * image.width
+                            }
                         }
                     }
                 }
 
-                hbox {
-                    textfield {
-                        alignment = Pos.CENTER_RIGHT
-                        text = "10"
-                        textProperty().onChange {
-                            if (it == null) {
-                                iconShrinkLimit.set(0.1)
-                            } else {
-                                it.trim()
-                                if (it.isDouble() && it.toDouble() > 10) {
-                                    if (it.toDouble() > 75) {
-                                        iconShrinkLimit.set(.75)
-                                    } else {
-                                        iconShrinkLimit.set(it.toDouble() / 100)
-                                    }
+                vbox {
+
+                    vbox {
+                        label {
+                            text = "Number of clicks to shrink Icon"
+                        }
+
+                        textfield {
+                            alignment = Pos.CENTER_RIGHT
+                            text = "1"
+
+                            textProperty().onChange {
+                                if (it == null) {
+                                    iconClicksToShrink.set(1)
                                 } else {
-                                    iconShrinkLimit.set(0.1)
+                                    it.trim()
+                                    if (it.isInt() && it.toInt() >= 1) {
+                                        iconClicksToShrink.set(it.toInt())
+                                    } else {
+                                        iconClicksToShrink.set(1)
+                                    }
+                                }
+                            }
+
+                            focusedProperty().onChange {
+                                if (!it) {
+                                    text = iconClicksToShrink.value.toString()
                                 }
                             }
                         }
-
-                        focusedProperty().onChange {
-                            if (!it) {
-                                text = (iconShrinkLimit.value * 100).toString()
-                            }
-                        }
                     }
 
-                    text {
-                        text = "%"
-                        font = Font.font(font.family, 24.0)
-                    }
+                    this += PercentInputField(
+                            "Starting Icon Size relative to Screen",
+                            iconStartSize,
+                            iconStartSize.value,
+                            .1
+                    )
+                    this += PercentInputField(
+                            "Smallest Icon Size relative to Screen",
+                            iconShrinkLimit,
+                            iconShrinkLimit.value,
+                            .1
+                    )
                 }
+
+            }
+
+            button("START") {
+                disableProperty().bind(!readyToStart)
             }
         }
     }
---
+++ src/main/kotlin/widgets/PercentInputField.kt
@@ -0,0 +1,66 @@
+package widgets
+
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.geometry.Pos
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
+import tornadofx.*
+
+class PercentInputField (
+        label : String,
+        input : SimpleDoubleProperty,
+        default : Double,
+        minimum : Double = 0.0,
+        maximum : Double = 1.0
+) : VBox() {
+    
+    init {
+
+        label {
+            text = label
+        }
+
+        hbox {
+            textfield {
+                alignment = Pos.CENTER_RIGHT
+
+                text = doubleToString(default * 100)
+
+                textProperty().onChange {
+                    if (it == null) {
+                        input.set(default)
+                    } else {
+                        it.trim()
+                        if (it.isDouble() && it.toDouble() > minimum) {
+                            if (it.toDouble() > maximum) {
+                                input.set(maximum)
+                            } else {
+                                input.set(it.toDouble() / 100)
+                            }
+                        } else {
+                            input.set(0.1)
+                        }
+                    }
+                }
+
+                focusedProperty().onChange {
+                    if (!it) {
+                        text = doubleToString(input.value * 100)
+                    }
+                }
+            }
+
+            text {
+                text = "%"
+                font = Font.font(font.family, 20.0)
+            }
+        }
+    }
+}
+
+fun doubleToString (value : Double) : String{
+    return if (value == value.toLong().toDouble())
+        String.format("%d", value.toLong())
+    else
+        String.format("%s", value)
+}
