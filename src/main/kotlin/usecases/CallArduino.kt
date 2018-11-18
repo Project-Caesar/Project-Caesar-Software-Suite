37d87d642a9e8cd72d33418a6a5b47209e821b58
Alvy01 <jordanalvarez01@gmail.com>
2018-11-18 16:16:03 -0500

    Corrected the value being pushed to the arduino from 5 to 1

--- src/main/kotlin/usecases/CallArduino.kt
+++ src/main/kotlin/usecases/CallArduino.kt
@@ -1,7 +1,7 @@
 package usecases
 
 fun callArduino(pid_num: String) {
-    val command = arrayOf("screen", "-S", pid_num, "-p", "0", "-X", "stuff", "\"5\"")
+    val command = arrayOf("screen", "-S", pid_num, "-p", "0", "-X", "stuff", "\"1\"")
     val echoProcess = Runtime.getRuntime().exec(command)
     echoProcess.waitFor()
     //Thread.sleep(1_000)
